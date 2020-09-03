b6a7f312e1a9a42d6926d86edb152f0b63e77bb5
alpenmilch <1836145762@qq.com>
2020-09-03 20:55:29 +0800

    Updates

--- precache-manifest.799450063a789595f8d619c377d25db2.js
+++ precache-manifest.799450063a789595f8d619c377d25db2.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b877baf1b39417cf43460b7f35a2ef22",
+    "revision": "385c9fbfe70aa29657ee27d11ad8104d",
     "url": "/Bigraph-front-end/index.html"
   },
   {
-    "revision": "b306ea6f984127cd926d",
+    "revision": "dc11afca6681fa8f757f",
     "url": "/Bigraph-front-end/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "43d8bdea2523942e5af8",
-    "url": "/Bigraph-front-end/static/js/2.d8fd5399.chunk.js"
+    "revision": "7fea5c2260386d6500f6",
+    "url": "/Bigraph-front-end/static/js/2.59afa571.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/Bigraph-front-end/static/js/2.d8fd5399.chunk.js.LICENSE.txt"
+    "url": "/Bigraph-front-end/static/js/2.59afa571.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b306ea6f984127cd926d",
-    "url": "/Bigraph-front-end/static/js/main.59edbcac.chunk.js"
+    "revision": "dc11afca6681fa8f757f",
+    "url": "/Bigraph-front-end/static/js/main.9e8ff7ca.chunk.js"
   },
   {
     "revision": "50da8033d57ab4ad1f87",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Bigraph-front-end/precache-manifest.2ee6a970cc62a79c36bb2f2711d32480.js"
+  "/Bigraph-front-end/precache-manifest.799450063a789595f8d619c377d25db2.js"
 );
 
 self.addEventListener('message', (event) => {
